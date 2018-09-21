946ba4dc556fdd6f7a2bf19e8a56a298aded1478
Mzamo <molimica@gmail.com>
2018-09-21 19:48:16 +0200

    Añadida linea y palabra que se mueve

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour {
 
     public List<string> wordList;
     public List<string> missingCharList;
+    public TextMesh word;
 
     private int wordIndex = 0;
     private string currentWord;
@@ -14,17 +15,13 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         currentWord = wordList[wordIndex].Replace(missingCharList[wordIndex].ToCharArray()[0], '_');
+        word.text = currentWord;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        word.transform.position += new Vector3(0, -1*Time.deltaTime, 0);
 	}
-
-    private void OnGUI()
-    {
-        GUI.Label(new Rect(Screen.width/2 - 100, Screen.height*1/3, 200, 40), currentWord);
-    }
 
     public void ReceiveLetter(string letter)
     {
@@ -32,7 +29,10 @@ public class GameManager : MonoBehaviour {
             Debug.Log("Bien, sabes leer!!!");
             wordIndex++;
             if (wordIndex < wordList.Count)
+            {
                 currentWord = wordList[wordIndex].Replace(missingCharList[wordIndex].ToCharArray()[0], '_');
+                word.text = currentWord;
+            }
             else
                 SceneManager.LoadScene(0);
         }
