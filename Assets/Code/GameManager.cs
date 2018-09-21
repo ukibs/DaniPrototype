97563ad15a19d907e76c9208a55725e5c0e2760e
ukibs <martincativiela@gmail.com>
2018-09-21 19:04:21 +0200

    Creación de proyecto

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public List<string> wordList;
+    public List<string> missingCharList;
+
+    private int wordIndex = 0;
+    private string currentWord;
+
+	// Use this for initialization
+	void Start () {
+        currentWord = wordList[wordIndex].Replace(missingCharList[wordIndex].ToCharArray()[0], '_');
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width/2 - 100, Screen.height*1/3, 200, 40), currentWord);
+    }
+
+    public void ReceiveLetter(string letter)
+    {
+        if (letter.Equals(missingCharList[wordIndex])){
+            Debug.Log("Bien, sabes leer!!!");
+            wordIndex++;
+            if (wordIndex < wordList.Count)
+                currentWord = wordList[wordIndex].Replace(missingCharList[wordIndex].ToCharArray()[0], '_');
+            else
+                SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("Burro!!!");
+        }
+    }
+}
