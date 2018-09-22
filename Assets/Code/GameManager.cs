88afe623df05cefc457a283d44c10b8a47199d2b
Mzamo <molimica@gmail.com>
2018-09-22 12:42:22 +0200

    Van saliendo en orden, se generan todas al principio y coge datos de xml

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -4,34 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+    public GameObject wordPrefab;
+    public float timeWait = 2.0f;
 
-    public List<string> wordList;
-    public List<string> missingCharList;
-    public TextMesh word;
-
-    private int wordIndex = 0;
-    private string currentWord;
+    private Queue<WordInfo> wordObjects;
+    private Queue<WordInfo> wordsInScreen;
+    private float timer;
 
 	// Use this for initialization
 	void Start () {
-        currentWord = wordList[wordIndex].Replace(missingCharList[wordIndex].ToCharArray()[0], '_');
-        word.text = currentWord;
+        string[] wordsFromXml = GameFunctions.GetTextXML("ZCS", "WORDS", "word");
+        string[] lettersFromXml = GameFunctions.GetTextXML("ZCS", "LETTERS", "letter");
+
+        int auxInt = 0;
+        wordObjects = new Queue<WordInfo>();
+        wordsInScreen = new Queue<WordInfo>(4);
+        foreach(string s in wordsFromXml)
+        {
+            WordInfo aux = Instantiate(wordPrefab).GetComponent<WordInfo>();
+            aux.Word = s;
+            aux.Letter = lettersFromXml[auxInt];
+            aux.gameObject.SetActive(false);
+            wordObjects.Enqueue(aux);
+            auxInt++;
+        }
+
+        AddWordInScreen();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        word.transform.position += new Vector3(0, -1*Time.deltaTime, 0);
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        foreach (WordInfo t in wordsInScreen)
+        {
+            t.transform.position += new Vector3(0, -1 * Time.deltaTime, 0);
+        }
+        if(timer > timeWait)
+        {
+            timer = 0;
+            AddWordInScreen();
+        }
 	}
 
     public void ReceiveLetter(string letter)
     {
-        if (letter.Equals(missingCharList[wordIndex])){
+        if (letter.Equals(wordsInScreen.Peek().Letter)){
             Debug.Log("Bien, sabes leer!!!");
-            wordIndex++;
-            if (wordIndex < wordList.Count)
+            if (wordsInScreen.Count > 1 || wordObjects.Count != 0)
             {
-                currentWord = wordList[wordIndex].Replace(missingCharList[wordIndex].ToCharArray()[0], '_');
-                word.text = currentWord;
+                wordsInScreen.Dequeue().gameObject.SetActive(false);
             }
             else
                 SceneManager.LoadScene(0);
@@ -40,5 +62,12 @@ public class GameManager : MonoBehaviour {
         {
             Debug.Log("Burro!!!");
         }
+    }
+
+    private void AddWordInScreen()
+    {
+        WordInfo addText = wordObjects.Dequeue().GetComponent<WordInfo>();
+        addText.gameObject.SetActive(true);
+        wordsInScreen.Enqueue(addText);
     }
 }
---
+++ Assets/Code/WordInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordInfo: MonoBehaviour {
+    string word;
+    string letter;
+
+	public string Word
+    {
+        get { return word; }
+        set { word = value;}
+    }
+
+    public string Letter
+    {
+        get { return letter; }
+        set { letter = value;
+            GetComponent<TextMesh>().text = word.Replace(letter.ToCharArray()[0], '_'); ;
+        }
+    }
+}
