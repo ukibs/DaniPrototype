c68f4a0d22b9f394e238a539d3c573c9c8070361
ukibs <martincativiela@gmail.com>
2018-10-31 11:31:30 +0100

    Añadido movimiento de camara con ratón en selector de nivel

---
+++ Assets/Code/CamLevelSelectorMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLevelSelectorMovement : MonoBehaviour {
+
+    private float previousMouseX;
+    private float previousMouseY;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousMouseX = Input.mousePosition.x;
+            previousMouseY = Input.mousePosition.y;
+        }
+        //
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 newPosition = transform.position;
+
+            newPosition.x += (previousMouseX - Input.mousePosition.x) * Time.deltaTime;
+            newPosition.y += (previousMouseY - Input.mousePosition.y) * Time.deltaTime;
+
+            transform.position = newPosition;
+
+            previousMouseX = Input.mousePosition.x;
+            previousMouseY = Input.mousePosition.y;
+        }
+	}
+
+    
+}
